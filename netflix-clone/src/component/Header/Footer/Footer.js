29a4d06a37959633478cd8843abe9b895aaf07b8
hawlet-yimam <yhawelet@gmail.com>
2026-08-01 10:20:07 +0300

    Footer

--- netflix-clone/src/component/Header/Footer/Footer.js
+++ netflix-clone/src/component/Header/Footer/Footer.js
@@ -1,9 +1,52 @@
 import React from 'react'
-
+import "./Footer.css";
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 export default function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+        <div className='footer_icons'>
+           <FacebookOutlinedIcon/>
+           <InstagramIcon/>
+           <YouTubeIcon/>
+        </div>
+        <div>
+          <ul>
+            <li>Audio Description</li>
+            <li>Investor Relations</li>
+            <li>Legal Notice</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>Cookie Preferences</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Gift Cards</li>
+            <li>Terms of Use</li>
+            <li>Corporate Information</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Media Center</li>
+            <li>Privacy</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className='Service_code'>
+          <p>Service Code</p>
+        </div>
+        <div className='copy_writer'>
+        &copy;2005-2026 Netflix,inc.
+        </div>
+      </div>
     </div>
   )
 }
